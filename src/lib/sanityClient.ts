01b05275082c10f36ad309e348014b38d24c0714
Md Rohidul Islam <rohidul209@gmail.com>
2022-07-27 00:24:44 +0600

    color-scheme to theme

--- src/lib/sanityClient.ts
+++ src/lib/sanityClient.ts
@@ -3,6 +3,6 @@ import { createClient } from "next-sanity";
 export default createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-  useCdn: process.env.NODE_ENV === "production",
+  useCdn: process.env.NEXT_PUBLIC_VERCEL_ENV === "production",
   apiVersion: "2021-10-21",
 });
--- src/stores/app.ts
+++ src/stores/app.ts
@@ -4,7 +4,7 @@ import { atom } from "jotai";
 export type ColorScheme = "dark" | "light";
 
 export const colorSchemeAtom = atomWithAsyncStorage<ColorScheme>(
-  "color-scheme",
+  "theme",
   "light"
 );
 export const showSidebarAtom = atom(false);
